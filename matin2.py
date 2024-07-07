21edf547fb7c58bafd0ab0bf62133d7869e750e2
Mhmad-matin <matin853godfather5@gmail.com>
2024-07-07 02:01:34 -0700

    محاسبه عددی

---
+++ matin2.py
@@ -0,0 +1,13 @@
+
+number = 50
+print("مقدار متغیر عددی", number)
+
+
+mianterm = 60
+paianterm = 40
+
+
+total_grade = mianterm + paianterm
+
+
+print("مجموع امتحان میان ترم و نهایی", mianterm, "+", paianterm, "=", total_grade)
